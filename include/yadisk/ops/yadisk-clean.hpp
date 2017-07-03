0e78c6c48aba19d6d6b2cc630ade61399d7ee7aa
alexandermlabs <alexandermlabs@yandex.ru>
2017-07-03 11:27:44 +0300

    Update yadisk-clean.hpp
    попытка исправления ошибки

--- include/yadisk/ops/yadisk-clean.hpp
+++ include/yadisk/ops/yadisk-clean.hpp
@@ -1,29 +1,37 @@
-using yadisk::ops::Tree;
-using yadisk::ops::Predicate;
-using yadisk::ops::Duplicate;
-using yadisk::Client;
-using std::string;
-using std::map;
+#ifndef YADISK_CLEAN_HPP
+#define YADISK_CLEAN_HPP
 
-using Resources = std::vector<Resource>;
- 
+#include <string>
+#include <map>
+
+#include <include/yadisk/ops/Tree.hpp>
+#include <include/yadisk/ops/Resource.hpp>
+#include <include/yadisk/ops/Predicate.hpp>
+#include <yadisk/client.hpp>
+
+// using в header стараемся не использовать, чтобы не возникло коллизии 
+// с другими проектами при подключении данного header-а.
+
 namespace yadisk 
 {
-namespace ops 
-{
-	void clear_duplicate(Client& client, Predicate pred = Hash)
+	namespace ops 
 	{
-		Map<string, Resources> duplicate = ops.find_duplicate(client);
-		
-		for(auto it = duplicate.begin(); it!= duplicate.end(); ++it)
+		void clear_duplicate(yadisk::Client& client, yadisk::ops::Predicate pred = yadisk::ops::Hash)
 		{
-			for(int i = 0; i < it->second.size(); ++i)
-				if(it->first != it->second[i].path)
+			std::map<std::string, std::vector<yadisk::ops::Resource>> duplicate = ops.find_duplicate(client);
+			for(auto it = duplicate.begin(); it != duplicate.end(); ++it)
+			{
+				for (auto p : it->second)
 				{
-					client.remove(it->second[i].path, true);
-					std::cout << "Duplicate if founded and deleted!";
+					if (it->first != p.path)
+					{
+						client.remove(p.path, true);
+						std::cout << "Duplicate founded and deleted!";
+					}
 				}
+			}
 		}
 	}
 }
-}
+
+#endif
